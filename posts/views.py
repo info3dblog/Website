c3685a33764a38bba208e1e28bdefe3fa41bd1b8
info3dblog <info3dblog@protonmail.com>
2018-10-18 18:55:41 +0200

    MAJ du 18/10/2018

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Post
+from django.template import loader
 
 def home(request):
 	return HttpResponse("""<h1>Info3DBlog</h1><p>This is a text to test</p>""")
+
+def index(request):
+    latest_posts_list = Post.objects.order_by('-id')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_posts_list': latest_posts_list,
+	}
+    return HttpResponse(template.render(context, request))
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at question %s." % post_id)
