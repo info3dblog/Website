13cb47a874af1322c752e3eedb8ebf8ea8ae4664
info3dblog <info3dblog@protonmail.com>
2018-11-03 22:34:56 +0100

    Ajout d'un petit coin de papier plié en haut à droite des pages

--- info3dblog/settings.py
+++ info3dblog/settings.py
@@ -122,7 +122,10 @@ STATIC_URL = '/static/'
 
 # MarkupField settings
 import markdown
+from mdx_gfm import GithubFlavoredMarkdownExtension
+
+markdown_github_like = markdown.Markdown(extensions=[GithubFlavoredMarkdownExtension()])
 
 MARKUP_FIELD_TYPES = (
-    ('markdown', markdown.markdown),
+    ('markdown', markdown_github_like.convert),
 )
